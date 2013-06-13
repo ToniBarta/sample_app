740b002cd6097ef62c2a4703a9324501b49d951a
ToniBarta <bartatoni@gmail.com>
2013-06-13 11:17:43 +0100

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,11 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should_not be_admin }
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -130,6 +135,15 @@ describe "remember token" do
     its(:remember_token) { should_not be_blank }
   end
 
+#### deleting a user #### making admin users
+describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
 
 end
